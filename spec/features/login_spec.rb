177b0a056a94240adceaedfc915a0bfdfa9b0c57
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-11 17:46:01 -0600

    Login testing beginning.

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "User Log In" do
+  it 'has a link to login a user' do 
+    visit '/'
+
+    expect(page).to have_link('Log In')
+
+    click_link('Log In')
+    expect(current_path).to eq('/login')
+  end
+
+  it 'has a form to fill to log in a user' do 
+    visit '/login'
+
+    expect(page).to have_content('Email:')
+    expect(page).to have_content('Password:')
+    expect(page).to have_button('Log In')
+
+    user = User.create(name: 'Eric Bachmann', email: 'bachmanity_rocks@gmail.com', password: 'jingyangsux')
+
+    fill_in :email, with: user.email 
+    fill_in :password, with: user.password 
+    
+    click_button('Log In')
+    expect(current_path).to eq(root_path)
+
+    expect(page).to have_content("Welcome, #{user.name}")
+
+  end
+end
