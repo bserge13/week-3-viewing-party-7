a62267f5385829a31ce920ce7afb84481f92a5cf
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-11 14:24:35 -0600

    Model: addition of has_secure_password to User model. Test: addition of testing to presence of a users password. Database: addition of password_disgest to Users table. All testing passes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User <ApplicationRecord
   validates_presence_of :email, :name 
   validates_uniqueness_of :email
   has_many :viewing_parties
+  validates_presence_of :password
+  has_secure_password
 end 
