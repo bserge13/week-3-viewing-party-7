c335622be219d50956a378d7db13dc7a0a0b3bc0
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-12 19:54:02 -0600

    Task 6, US 4, Pt 2: refactor of viewingparty new controller action to search for conditional of a current_user in order to make a new viewing party. All testing passes.

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,23 +1,26 @@
 class ViewingPartiesController < ApplicationController 
-  def new
-    @user = User.find(params[:user_id])
-    @movie = Movie.find(params[:movie_id])
+	def new
+		if current_user
+			@user = User.find(params[:user_id])
+			@movie = Movie.find(params[:movie_id])
+		else 
+			user = User.find(params[:user_id])
+      movie = Movie.find(params[:movie_id])
+
+			redirect_to movie_path(user, movie)
+			flash[:error] = 'You must be logged in or registered to create a movie party'
+		end 
   end 
   
   def create 
-    if current_user
-      user = User.find(params[:user_id])
-      user.viewing_parties.create(viewing_party_params)
-      redirect_to "/users/#{params[:user_id]}"
-    else 
-      redirect_to movie_path(user, movie)
-      flash[:error] = 'You must be logged in or registered to create a movie party'
-    end 
+    user = User.find(params[:user_id])
+    user.viewing_parties.create(viewing_party_params)
+    redirect_to "/users/#{params[:user_id]}"
   end 
 
-  private 
+	private 
 
-  def viewing_party_params 
-    params.permit(:movie_id, :duration, :date, :time)
-  end 
+	def viewing_party_params 
+		params.permit(:movie_id, :duration, :date, :time)
+	end 
 end 
